455f26b383e5b5084cdc9ae8091ee69fe746f3d3
Damianus Deni <damianus.deni@gmail.com>
2020-11-22 14:51:39 +0700

    update team assignment logic

--- Modul 1 - Introduction to Data Science & Python - Working Files/20201123_intro_to_python_dd_mini_application.py
+++ Modul 1 - Introduction to Data Science & Python - Working Files/20201123_intro_to_python_dd_mini_application.py
@@ -1,25 +1,6 @@
-# data = [
-#     {
-#         "userId": 1,
-#         "id": 1,
-#         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-#         "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-#     },
-#     {
-#         "userId": 1,
-#         "id": 2,
-#         "title": "qui est esse",
-#         "body": "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
-#     },
-#     {
-#         "userId": 1,
-#         "id": 3,
-#         "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-#         "body": "et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut"
-#     }]
-
-car_list = ["mustang", "ford", "volkswagen",
-            "ferrari", "lamborghini", "porsche", "BMW"]
+# car_list = ["mustang", "ford", "volkswagen",
+#             "ferrari", "lamborghini", "porsche", "BMW"]
+car_list = []
 
 try:
     password = "pw9876"
@@ -43,176 +24,157 @@ try:
                 trial += 1
                 print(f"Wrong password. You cannot enter any more password")
             else:
-                system = ""
-                while system != "exit":
-                    print("Password is correct. Login is successful")
+                print("Password is correct. Login is successful")
+                exit_command = ""
+                while exit_command != "exit" or exit_command == "1":
                     print("#"*70)
                     print("\t\t\tWELCOME TO APPLICATION")
-                    print("#"*70)
+                    print("#"*70, '\n')
+                    print("\t\t===DATA===")
+                    for n, i in enumerate(car_list):
+                        print(f"\t\t{n+1}. {i}")
+                    print('\n', "+-"*35, '\n')
+                    print("\t\t===INSTRUCTIONS===")
                     print('''\t\tSelect the following instructions:\b
-                        - "print" to print item\b
-                        - "add" to add item\b
-                        - "update" to udpate item\b
-                        - "delete" to delete item\b
-                        - "exit" to exit the application\b
-                        ''')
+                    - "print" to print item\b
+                    - "add" to add item\b
+                    - "update" to udpate item\b
+                    - "delete" to delete item\b
+                    - "exit" to exit the application\b''')
                     instruction = str(
                         input('\t\t--->Enter your selected instruction: '))
 
                     if instruction == "print":
-                        print("-"*70)
-                        print('\t\tPRINT MENU')
-                        input_item = str(
-                            input("\t\t---> Enter item name: "))
-                        if input_item in car_list:
-                            index = car_list.index(input_item)
-                            print(car_list[index])
-                        elif input_item not in car_list:
-                            print("\t\tthe item is not in the list")
-                            print("\t\t'please check your item in this list'")
-                            for n, i in enumerate(car_list):
-                                print(f"\t\t{n+1}. {i}")
-                        else:
-                            print("'you do not enter the required inputs'")
+
+                        exit_command = ""
+                        while exit_command != "1":
+                            print("-"*70)
+                            print('\t\tPRINT MENU')
+                            if len(car_list) == 0:
+                                print("\t\tlist is empty")
+                            elif len(car_list) > 0:
+                                for n, i in enumerate(car_list):
+                                    print(f"\t\t{n+1}. {i}")
+                            exit_command = str(
+                                input(f"1. back to main menu: (1) \n2. repeat menu {instruction}: (2) \n--->"))
                         print("-"*70)
                     elif instruction == "add":
-                        print('\t\tADD MENU')
-                        input_item = str(
-                            input("\t\t---> Enter item name: "))
-                        if input_item in car_list:
-                            index = car_list.index(input_item)
-                            if type(input_item) == type(car_list[index]):
-                                command = str(input('''\t\t---> Data has been registered.\b
-                                        Do you want to add?(Y/N): '''))
+                        exit_command = ""
+                        while exit_command != "1":
+                            print('\t\tADD MENU')
+                            input_item = str(
+                                input("\t\t---> Enter item name: "))
+                            if input_item in car_list:
+                                index = car_list.index(input_item)
+                                if type(input_item) == type(car_list[index]):
+                                    command = str(
+                                        input("\t\t---> Data has been registered.\n \t\tDo you want to add?(Y/N): "))
+                                    if command == "Y":
+                                        car_list.append(input_item)
+                                        print("\t\t'item has been added'")
+                                    else:
+                                        print("\t\t'item is not added'")
+                                    for n, i in enumerate(car_list):
+                                        print(f"\t\t{n+1}. {i}")
+                                else:
+                                    print("data format is incorrect")
+                            elif input_item not in car_list:
+                                command = str(
+                                    input("\t\t---> Data is not registered.\n \t\tDo you want to add data?(Y/N): "))
                                 if command == "Y":
                                     car_list.append(input_item)
-                                    print("\t\t'item has been added'")
+                                    print(f"\t\t'item has been added'")
                                 else:
                                     print("\t\t'item is not added'")
                                 for n, i in enumerate(car_list):
                                     print(f"\t\t{n+1}. {i}")
                             else:
-                                print("data format is incorrect")
-                        elif input_item not in car_list:
-                            command = str(input('''\t\t---> Data is not registered.\b
-                                                Do you want to add data?(Y/N): '''))
-                            if command == "Y":
-                                car_list.append(input_item)
-                                print(f"\t\t'item has been added'")
-                            else:
-                                print("\t\t'item is not added'")
-                            for n, i in enumerate(car_list):
-                                print(f"\t\t{n+1}. {i}")
-                        else:
-                            print("\t\t'the item is not in the list'")
-                            print("\t\t'please check your item in this list'")
-                            for n, i in enumerate(car_list):
-                                print(f"\t\t{n+1}. {i}")
+                                print("\t\t'the item is not in the list'")
+                                print("\t\t'please check your item in this list'")
+                                for n, i in enumerate(car_list):
+                                    print(f"\t\t{n+1}. {i}")
+
+                            exit_command = str(
+                                input(f"1. back to main menu: (1) \n2. repeat menu {instruction}: (2) \n--->"))
 
                         print("-"*70)
                     elif instruction == "update":
-                        print('\t\tUPDATE MENU')
-                        input_item = str(
-                            input("\t\t---> Enter item name to update: "))
-                        if input_item in car_list:
-                            index = car_list.index(input_item)
-                            if type(input_item) == type(car_list[index]):
-                                update_item = str(input('''\t\t---> Data has been registered.\b
-                                        Enter the update?: '''))
-                                car_list[index] = update_item
-                                print("\t\t'data has been updated'")
+                        exit_command = ""
+                        while exit_command != "1":
+                            print('\t\tUPDATE MENU')
+                            input_item = str(
+                                input("\t\t---> Enter item name to update: "))
+                            if input_item in car_list:
+                                index = car_list.index(input_item)
+                                if type(input_item) == type(car_list[index]):
+                                    update_item = str(
+                                        input("\t\t---> Data has been registered.\n \t\tEnter the update?: "))
+                                    car_list[index] = update_item
+                                    print("\t\t'data has been updated'")
+                                    for n, i in enumerate(car_list):
+                                        print(f"\t\t{n+1}. {i}")
+                                else:
+                                    print("'data format is incorrect'")
+                            else:
+                                print("\t\t'the item is not in the lis't")
+                                print("\t\t'please check your item in this list'")
                                 for n, i in enumerate(car_list):
                                     print(f"\t\t{n+1}. {i}")
-                            else:
-                                print("'data format is incorrect'")
-                        else:
-                            print("\t\t'the item is not in the lis't")
-                            print("\t\t'please check your item in this list'")
-                            for n, i in enumerate(car_list):
-                                print(f"\t\t{n+1}. {i}")
+
+                            exit_command = str(
+                                input(f"1. back to main menu: (1) \n2. repeat menu {instruction}: (2) \n--->"))
 
                         print("-"*70)
                     elif instruction == "delete":
-                        print('\t\tDELETE MENU')
-                        input_item = str(
-                            input("\t\t---> Enter item name to update: "))
-                        if input_item in car_list:
-                            index = car_list.index(input_item)
-                            if type(input_item) == type(car_list[index]):
-                                # update_item = str(input('''\t\t---> Data has been registered.\b
-                                #         Enter the item to delete?: '''))
-                                car_list.remove(input_item)
-                                print(
-                                    f"\t\t'-{input_item}- has been deleted'")
+                        exit_command = ""
+                        while exit_command != "1":
+                            print('\t\tDELETE MENU')
+                            input_item = str(
+                                input("\t\t---> Enter item name to delete: "))
+                            for i in car_list:
+                                if i == input_item:
+                                    car_list.remove(input_item)
+                                # index = car_list.index(input_item)
+                                # if type(input_item) == type(car_list[index]):
+                                #     car_list.remove(input_item)
+                                #     print(
+                                #         f"\t\t'-{input_item}- has been deleted'")
+                                #     for n, i in enumerate(car_list):
+                                #         print(f"\t\t{n+1}. {i}")
+                                # else:
+                                #     print("'data format is incorrect'")
+                            else:
+                                print("\t\t'the item is not in the list'")
+                                print("\t\t'please check your item in this list'")
                                 for n, i in enumerate(car_list):
                                     print(f"\t\t{n+1}. {i}")
-                            else:
-                                print("'data format is incorrect'")
-                        else:
-                            print("\t\t'the item is not in the list'")
-                            print("\t\t'please check your item in this list'")
-                            for n, i in enumerate(car_list):
-                                print(f"\t\t{n+1}. {i}")
+
+                            exit_command = str(
+                                input(f"1. back to main menu: (1) \n2. repeat menu {instruction}: (2) \n--->"))
+
                         print("-"*70)
                     elif instruction == "exit":
                         print('\t\tEXIT')
                         print("-"*70)
                         break
                     else:
-                        print("you dont enter the correct instruction")
+                        print("\t\t'you do not enter the correct instruction'")
                         print("-"*70)
 
-                    system = str(
-                        input('''\t1. type any keyword to return to main menu
-\t2. type "exit" to quit application
-\t--->'''))
+                    # system = str(
+                    #     input("\t1. type any keyword to return to main menu\n\t2. type 'exit' to quit application\n\t--->"))
 except:
     print("\t\terror occurs")
 
-
-# instruction = str(input('''Select the following instructions:\b
-#                             - "print" to print item\b
-#                             - "add" to add item\b
-#                             - "update" to udpate item\b
-#                             - "delete" to delete item\b
-#                             - "exit" to exit the application\n
-
-#                             Enter your selected instruction: '''))
-
-# if instruction == "print":
-#     print('print')
-# elif instruction == "add":
-#     print('add')
-# elif instruction == "update":
-#     print('update')
-# elif instruction == "delete":
-#     print('delete')
-# elif instruction == "exit":
-#     print('exit')
-# else:
-#     print("you dont enter the correct instruction")
-
-
-# try:
-#     input_item = str(input("Enter item name: "))
-#     instruction = str(input('''Select the following instructions:\b
-#                             - "print" to print item\b
-#                             - "add" to add item\b
-#                             - "update" to udpate item\b
-#                             - "delete" to delete item\b
-#                             - "exit" to exit the application\n
-
-#                             Enter your selected instruction: '''))
-#     # 1 Read
-#     if input_item in car_list:
-#         index = car_list.index(input_item)
-#         if instruction == "print":
-#             print(car_list[index])
-#         elif instruction == "add":
-#             print("add instruction")
-#     elif input_item not in car_list:
-#         print("the item is not in the list")
-#     else:
-#         print("you do not enter the required inputs")
-# except:
-#     print('error')
+    # input_item = str(
+    #     input("\t\t---> Enter item name: "))
+    # if input_item in car_list:
+    #     index = car_list.index(input_item)
+    #     print(car_list[index])
+    # elif input_item not in car_list:
+    #     print("\t\tthe item is not in the list")
+    #     print("\t\t'please check your item in this list'")
+    #     for n, i in enumerate(car_list):
+    #         print(f"\t\t{n+1}. {i}")
+    # else:
+    #     print("'you do not enter the required inputs'")
