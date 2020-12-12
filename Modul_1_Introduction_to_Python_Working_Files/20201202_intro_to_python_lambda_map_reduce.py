6cd72ea9297a428b8e24b815fb63d784c2b4cfc3
Damianus Deni <damianus.deni@gmail.com>
2020-12-12 21:50:50 +0700

    update assignment function

--- Modul_1_Introduction_to_Python_Working_Files/20201202_intro_to_python_lambda_map_reduce.py
+++ Modul_1_Introduction_to_Python_Working_Files/20201202_intro_to_python_lambda_map_reduce.py
@@ -143,7 +143,7 @@ except:
 '''
 
 print("#"*50)
-
+'''
 # 3.
 # -Buat Algoritma Stats-
 # Buat List
@@ -258,15 +258,156 @@ input_number = [56, 220, 4, 45, 13, 10, 45, 6, 10, 100, 1]
 print(med(input_number))
 print(mean(input_number))
 print(mod(input_number))
+'''
+print("#"*50)
 
-# def input_list():
-#     input_list = []
-#     list_length = int(input("length of list: "))
-#     for i in range(0, list_length):
-#         index = int(input(f"index {i+1}: "))
-#         input_list.append(index)
-#     return input_list
+# 4. Buat Return Function untuk Spin angka
+
+# Ada Deret Angka(List dalam list)
+# [[1 2 3 4 5],
+#  [6 7 8 9 10],
+#  [11 12 13 14 15],
+#  [16 17 18 19 20],
+#  [21 22 23 24 25]]
+
+# Input:
+# Masukkan Angka 1-3
+
+# Pilihan 1:
+# [[21, 16, 11, 6, 1],
+#  [22, 17, 12, 7, 2],
+#  [23, 18, 13, 8, 3],
+#  [24, 19, 14, 9, 4],
+#  [25, 20, 15, 10, 5]]
+
+# Pilihan 2:
+# [[25, 24, 23, 22, 21],
+#  [20, 19, 18, 17, 16],
+#  [15, 14, 13, 12, 11],
+#  [10, 9, 8, 7, 6],
+#  [5, 4, 3, 2, 1]]
+
+# Pilihan 3:
+# [[5, 10, 15, 20, 25],
+#  [4, 9, 14, 19, 24],
+#  [3, 8, 13, 18, 23],
+#  [2, 7, 12, 17, 22],
+#  [1, 6, 11, 16, 21]]
+
+print("#"*50)
+
+'''
+# 5.
+# Buat Return Function
+# Kalkulator: (+, -, /, *)
+# Inputan
+# input angka 1: 8
+# input angka 2: 10
+# masukkan operator: +
+
+# Output: Hasil Penjumlahan dari 8 + 10 = 18
 
 
-# list_number = input_list()
-# print(list_number)
+def addition(x, y):
+    result = x + y
+    return result
+
+
+def substration(x, y):
+    result = x - y
+    return result
+
+
+def multiply(x, y):
+    result = x * y
+    return result
+
+
+def division(x, y):
+    result = x / y
+    return result
+
+
+def calculation(x, y, operator):
+    if operator == "addition" or operator == "+":
+        result = addition(x, y)
+        return result
+    elif operator == "substration" or operator == "-":
+        result = substration(x, y)
+        return result
+    elif operator == "multiply" or operator == "*" or operator == "x":
+        result = multiply(x, y)
+        return result
+    elif operator == "division" or operator == "/" or operator == ":":
+        result = division(x, y)
+        return result
+    else:
+        return "please select the operator"
+
+
+def calculator():
+    try:
+        x = int(input("type number 1: "))
+        y = int(input("type number 2: "))
+        operator = input("select the operator: ")
+
+        result = calculation(x, y, operator)
+        output = f"the result of {x} {operator} {y} is {result}"
+
+        return output
+    except:
+        return "calculation error, please select the correct number and operator"
+
+
+print(calculator())
+'''
+print("#"*50)
+
+# 6.
+# Buat Def Function
+# Fizz Buzz
+
+# Input: Masukkan Angka:
+# Output:
+# ANgka dari input user akan menjadi range dari 1 - inputan tersebut
+# Output nya
+# akan dicek seluruh angka tersebut
+# kemudian akan di print
+# angka yg habis dibagi 3 diubah menjadi Fizz
+# angka yg habis dibagi 5 diubah menjadi Buzz
+# angka yg habis dibagi 3 dan 5 menjadi FizzBuzz
+# angka lain akan dicetak normal
+
+print("#"*50)
+
+# 7.
+# Buat Def Function
+# Caesar Cipher
+# Masukkan kata: Joni
+# masukkan angka: 2
+# Output nya: lqpk
+# j + 2 = l
+# o + 2 = q
+# n + 2 = p
+# i + 2 = k
+
+# masukkan kata: Joni
+# masukkan angka: -2
+# Output: imlg
+
+print("#"*50)
+
+
+# 8.
+# Konversi Romawi
+# Buat Return Function
+# Gunakan Dict
+# Batasan Maksimal 4000
+# Keluar notif: Angka diluar jangkauan
+# Encoder - Decoder Angka Romawi
+
+# Silakan Masukkan Angka: 2018
+# Output nya: MMXVIII
+
+# Silakan Masukkan angka: MMXVIII
+# Output nya: 2018
