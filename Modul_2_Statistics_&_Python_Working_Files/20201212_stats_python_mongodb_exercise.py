8fe445a22ccf015e82cdd0e5a04ce8dd255aa185
Damianus Deni <damianus.deni@gmail.com>
2020-12-20 19:03:47 +0700

    update mongodb task

---
+++ Modul_2_Statistics_&_Python_Working_Files/20201212_stats_python_mongodb_exercise.py
@@ -0,0 +1,34 @@
+# 1) display all documents
+db.restaurants.find()
+
+# 2) display the fields restaurant_id, name, borough and cuisine
+db.restaurants.find({}, {restaurant_id: 1, name: 1,
+                         borough: 1, cuisine: 1}).pretty()
+
+# 3) display the fields restaurant_id, name, borough and cuisine, but exclude the field _id
+db.restaurants.find({}, {_id: 0, restaurant_id: 1, name: 1,
+                         borough: 1, cuisine: 1}).pretty()
+
+# 4)  display the fields restaurant_id, name, borough and zip code, but exclude the field _id
+db.restaurants.find({}, {_id: 0, restaurant_id: 1, name: 1,
+                         borough: 1, "address.zipcode": 1}).pretty()
+
+# 5) display all the restaurant which is in the borough Bronx
+db.restaurants.find({"borough": "Bronx"}).pretty()
+
+# 6) to display the first 5 restaurant which is in the borough Bronx
+db.restaurants.find({"borough": "Bronx"}).limit(5)
+
+# 7) display the next 5 restaurants after skipping first 5 which are in the borough Bronx
+db.restaurants.find({'borough': 'Bronx'}).limit(5).skip(5)
+
+db.restaurants.find({}, {_id: 0, name: 1}).limit(5).skip(5)
+
+# 8)  find the restaurants who achieved a score more than 90
+db.restaurants.find({grades: {$elemMatch: {"score": {$gt: 90}}}}).pretty()
+
+# 9) find the restaurants that achieved a score, more than 80 but less than 100
+db.restaurants.find({$and: [{grades: {$elemMatch: {"score": {$gt: 80}}}}, {grades: {$elemMatch: {'score': {$lt: 100}}}}]}).pretty() - -> need to be checked
+
+# 10) find the restaurants which locate in latitude value less than -95.754168
+db.restaurants.find({"address.coord": {$lt: -95.754168}}).pretty()
